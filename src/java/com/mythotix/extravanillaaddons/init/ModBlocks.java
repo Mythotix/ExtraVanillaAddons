dfe0602b038c9022d43b3b53d0ea2f16d60c9526
Mythotix <tdogg10@hotmail.com>
2014-10-08 01:21:20 -0500

    Playing with Blocks!

--- src/java/com/mythotix/extravanillaaddons/init/ModBlocks.java
+++ src/java/com/mythotix/extravanillaaddons/init/ModBlocks.java
@@ -1,8 +1,10 @@
 package com.mythotix.extravanillaaddons.init;
 
 import com.mythotix.extravanillaaddons.block.BlockStoneStairs;
+import com.mythotix.extravanillaaddons.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks
 {
     public static final BlockStoneStairs stoneStairs = new BlockStoneStairs();
