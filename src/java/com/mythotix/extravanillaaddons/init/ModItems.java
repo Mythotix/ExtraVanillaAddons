45aa953614b9ae06ce3a8459392e131de860d764
Mythotix <tdogg10@hotmail.com>
2014-10-08 01:22:38 -0500

    Playing with Blocks!

--- src/java/com/mythotix/extravanillaaddons/init/ModItems.java
+++ src/java/com/mythotix/extravanillaaddons/init/ModItems.java
@@ -3,8 +3,10 @@ package com.mythotix.extravanillaaddons.init;
 import com.mythotix.extravanillaaddons.item.ItemDiamondStar;
 import com.mythotix.extravanillaaddons.item.ItemEVA;
 import com.mythotix.extravanillaaddons.item.ItemNetherStarFragment;
+import com.mythotix.extravanillaaddons.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModItems
 {
     public static final ItemEVA diamondStar = new ItemDiamondStar();
