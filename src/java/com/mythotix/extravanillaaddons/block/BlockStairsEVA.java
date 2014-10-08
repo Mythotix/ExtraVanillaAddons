5faa24dc68e7cb650d457b59f38bd4ec9cb3fd40
Mythotix <tdogg10@hotmail.com>
2014-10-08 01:36:15 -0500

    Playing with Blocks!

---
+++ src/java/com/mythotix/extravanillaaddons/block/BlockStairsEVA.java
@@ -0,0 +1,37 @@
+package com.mythotix.extravanillaaddons.block;
+
+import com.mythotix.extravanillaaddons.creativetab.CreativeTabEVA;
+import com.mythotix.extravanillaaddons.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockStairs;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+public class BlockStairsEVA extends BlockStairs
+{
+    public BlockStairsEVA(Block p_i45428_1_, int p_i45428_2_)
+    {
+        super(p_i45428_1_, p_i45428_2_);
+        this.setCreativeTab(CreativeTabEVA.EVA_TAB);
+        this.setLightOpacity(0);
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("tile.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        blockIcon = iconRegister.registerIcon(String.format("%s", getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+}
--- src/java/com/mythotix/extravanillaaddons/block/BlockStoneStairs.java
+++ src/java/com/mythotix/extravanillaaddons/block/BlockStoneStairs.java
@@ -1,12 +1,15 @@
 package com.mythotix.extravanillaaddons.block;
 
+import com.mythotix.extravanillaaddons.creativetab.CreativeTabEVA;
 import com.mythotix.extravanillaaddons.reference.Names;
+import net.minecraft.block.BlockStone;
+import net.minecraft.init.Blocks;
 
-public class BlockStoneStairs extends BlockEVA
+public class BlockStoneStairs extends BlockStairsEVA
 {
     public BlockStoneStairs()
     {
-        super();
+        super(Blocks.stone, 0);
         this.setBlockName(Names.Blocks.STONE_STAIRS);
     }
 }
